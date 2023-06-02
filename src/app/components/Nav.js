4fc5740cabf579a4b00881a9e41a40aa70cb0db0
Shakib-Shajid <shakibalshajid1682@gmail.com>
2023-06-02 17:05:18 +0600

    navbar

---
+++ src/app/components/Nav.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "@/app/styles/style.css";
+
+const Nav = () => {
+  return (
+    <div class="nav">
+      <ul>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Service</a>
+        </li>
+        <li>
+          <a href="">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Nav;
